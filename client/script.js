e39fb8d676872001d92ddcb4bd464e08354e8d79
radoslawgora <101334937+radoslawgora@users.noreply.github.com>
2023-03-27 21:18:24 +0200

    Update script.js
    change server address

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
 
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5001', {
+    const response = await fetch('https://ai-chat-5s7j.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
